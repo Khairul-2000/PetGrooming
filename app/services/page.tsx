efbc5355f530ace2dadbab33769e9fb6148dc1ce
Khairul-2000 <kahirulislam135@gmail.com>
2025-01-11 21:41:12 +0600

    page modification

---
+++ app/services/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Services from "../_componets/Services";
+
+const page = () => {
+  return (
+    <div>
+      <Services />
+    </div>
+  );
+};
+
+export default page;
